e34063c673d8ccc69064deb224b321dec147798b
Nicholas Sihite <nicholassihite05@gmail.com>
2024-04-01 13:57:59 +0700

    feat: Shift Key scancode

--- src/keyboard.c
+++ src/keyboard.c
@@ -25,13 +25,45 @@ const char keyboard_scancode_1_to_ascii_map[256] = {
       0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
 };
 
+const char keyboard_scancode_shift_to_ascii_map[256] = {
+      0, 0x1B, '!', '@', '#', '$', '%', '^',  '&', '*', '(',  ')',  '_', '+', '\b', '\t',
+    'Q',  'W', 'E', 'R', 'T', 'Y', 'U', 'I',  'O', 'P', '{',  '}', '\n',   0,  'A',  'S',
+    'D',  'F', 'G', 'H', 'J', 'K', 'L', ':', '\"', '`',   0, '\\',  'Z', 'X',  'C',  'V',
+    'B',  'N', 'M', '<', '>', '?',   0, '*',    0, ' ',   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0, '-',    0,    0,   0,  '+',    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+      0,    0,   0,   0,   0,   0,   0,   0,    0,   0,   0,    0,    0,   0,    0,    0,
+};
+
 void keyboard_isr(void) {
     uint8_t scancode = in(KEYBOARD_DATA_PORT);
+    static bool isShift = false;
     // TODO : Implement scancode processing
     // if(keyboard_state.keyboard_input_on){
     //   keyboard_state.keyboard_buffer = keyboard_scancode_1_to_ascii_map[scancode];
     //   pic_ack(1);
     // }
+
+    if (((scancode == 0x2a) || (scancode == 0x36)) && (!isShift)){ // Memeriksa apakah scancode adalah shift hold
+        isShift = true;
+        pic_ack(1);
+        return;
+    } else if (((scancode == 0xaa) || (scancode == 0xb6)) && (isShift)){ // Memeriksa apakah scancode adalah shift release
+        isShift = false;
+        pic_ack(1);
+        return;
+    }
+
     if (keyboard_state.keyboard_input_on) {
         // Memeriksa apakah scancode merupakan tombol "delete"
         if (scancode == 0xe) { // Jika scancode adalah untuk tombol "delete"
@@ -39,7 +71,11 @@ void keyboard_isr(void) {
             keyboard_state.keyboard_buffer = scancode;
         } else {
             // Memasukkan karakter sesuai dengan map scancode to ASCII ke dalam buffer
-            keyboard_state.keyboard_buffer = keyboard_scancode_1_to_ascii_map[scancode];
+            if (isShift){ // Jika shift
+                keyboard_state.keyboard_buffer = keyboard_scancode_shift_to_ascii_map[scancode];
+            } else { // Jika bukan shift
+                keyboard_state.keyboard_buffer = keyboard_scancode_1_to_ascii_map[scancode];
+            }
         }
         
         // Mengakui interrupt dari PIC
