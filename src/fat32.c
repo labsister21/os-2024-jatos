4e7866d4b0e3a715a86a115047bc05a20f15d1c7
Muhammad Zaki <muhammadzaki9@ymail.com>
2024-04-02 20:18:02 +0700

    feat: add init_directory

--- src/fat32.c
+++ src/fat32.c
@@ -38,7 +38,28 @@ uint32_t cluster_to_lba(uint32_t cluster){
  * @param parent_dir_cluster Parent directory cluster number
  */
 void init_directory_table(struct FAT32DirectoryTable *dir_table, char *name, uint32_t parent_dir_cluster){
+     driver_state.dir_table_buf.table[0].user_attribute = UATTR_NOT_EMPTY;
+    driver_state.dir_table_buf.table[1].user_attribute = UATTR_NOT_EMPTY;
+    driver_state.dir_table_buf.table[1].attribute = ATTR_SUBDIRECTORY;
 
+    for(int i = 0 ; i < 8 ; i++){
+        driver_state.dir_table_buf.table[0].name[i] = name[i];
+    }
+
+    for(int i = 0 ; i < 3;i++){
+        driver_state.dir_table_buf.table[0].ext[i] = '\0';
+    }
+
+    for(int i = 0 ;i < 8; i++){
+        driver_state.dir_table_buf.table[1].name[i] = '\0';
+    }
+
+    for(int i = 0 ; i < 3;i++){
+        driver_state.dir_table_buf.table[1].ext[i] = '\0';
+    }
+
+    driver_state.dir_table_buf.table[1].cluster_low = parent_dir_cluster & 0xFFFF;
+    driver_state.dir_table_buf.table[1].cluster_high = parent_dir_cluster >> 16;
 }
 
 /**
