07266a89051232b65b7eb14d78800ef3b7d3b9de
Nicholas Sihite <nicholassihite05@gmail.com>
2024-05-20 16:07:40 +0700

    FINAL CHANGE

--- src/kernel.c
+++ src/kernel.c
@@ -56,54 +56,54 @@ void kernel_setup(void) {
 
     scheduler_init();
 
-    char text2[264] = "Hololive Production is a virtual YouTuber agency owned by Japanese tech entertainment company Cover Corporation. In addition to acting as a multi-channel network, Hololive Production also handles licensing, merchandising, music production and concert organization.";
-    struct FAT32DriverRequest request2 = {
-        .buf                   = text2,
-        .name                  = "hololive",
-        .ext                   = "txt",
-        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-        .buffer_size           = 266,
-    };
-    write(request2);
+    // char text2[264] = "Hololive Production is a virtual YouTuber agency owned by Japanese tech entertainment company Cover Corporation. In addition to acting as a multi-channel network, Hololive Production also handles licensing, merchandising, music production and concert organization.";
+    // struct FAT32DriverRequest request2 = {
+    //     .buf                   = text2,
+    //     .name                  = "hololive",
+    //     .ext                   = "txt",
+    //     .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+    //     .buffer_size           = 266,
+    // };
+    // write(request2);
 
-    char text3[218] = "A VTuber or virtual YouTuber is an online entertainer who uses a virtual avatar generated using computer graphics. Real-time motion capture software or technology are often—but not always—used to capture movement.";
-    struct FAT32DriverRequest request3 = {
-        .buf                   = text3,
-        .name                  = "vtuber\0\0",
-        .ext                   = "txt",
-        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-        .buffer_size           = 218,
-    };
-    write(request3);
+    // char text3[218] = "A VTuber or virtual YouTuber is an online entertainer who uses a virtual avatar generated using computer graphics. Real-time motion capture software or technology are often—but not always—used to capture movement.";
+    // struct FAT32DriverRequest request3 = {
+    //     .buf                   = text3,
+    //     .name                  = "vtuber\0\0",
+    //     .ext                   = "txt",
+    //     .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+    //     .buffer_size           = 218,
+    // };
+    // write(request3);
 
-    char text4[292] = "Hoshimachi Suisei is a Japanese virtual YouTuber. She began posting videos as an independent creator in March 2018. In May 2019, she became affiliated with Hololive Production through their newly created music label, INoNaKa Music, before joining the agency's main branch later the same year.";
-     struct FAT32DriverRequest request4 = {
-        .buf                   = text4,
-        .name                  = "suisei\0\0",
-        .ext                   = "txt",
-        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-        .buffer_size           = 292,
-    };
-    write(request4);
+    // char text4[292] = "Hoshimachi Suisei is a Japanese virtual YouTuber. She began posting videos as an independent creator in March 2018. In May 2019, she became affiliated with Hololive Production through their newly created music label, INoNaKa Music, before joining the agency's main branch later the same year.";
+    //  struct FAT32DriverRequest request4 = {
+    //     .buf                   = text4,
+    //     .name                  = "suisei\0\0",
+    //     .ext                   = "txt",
+    //     .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+    //     .buffer_size           = 292,
+    // };
+    // write(request4);
 
-    struct FAT32DriverRequest request5 = {
-        .name                  = "fotozeta",
-        .ext                   = "\0\0\0",
-        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-        .buffer_size           = 0,
-    };
-    write(request5);
+    // struct FAT32DriverRequest request5 = {
+    //     .name                  = "fotozeta",
+    //     .ext                   = "\0\0\0",
+    //     .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+    //     .buffer_size           = 0,
+    // };
+    // write(request5);
 
-    char text6[208] =  "Vestia Zeta adalah YouTuber Virtual wanita Indonesia yang terkait dengan hololive, memulai debutnya sebagai bagian dari cabang VTubers generasi ketiga Indonesia (ID) bersama Kaela Kovalskia dan Kobo Kanaeru.";
+    // char text6[208] =  "Vestia Zeta adalah YouTuber Virtual wanita Indonesia yang terkait dengan hololive, memulai debutnya sebagai bagian dari cabang VTubers generasi ketiga Indonesia (ID) bersama Kaela Kovalskia dan Kobo Kanaeru.";
 
-    struct FAT32DriverRequest request6 = {
-        .buf                   = text6,
-        .name                  = "zeta\0\0\0\0",
-        .ext                   = "txt",
-        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
-        .buffer_size           = 208,
-    };
-    write(request6);
+    // struct FAT32DriverRequest request6 = {
+    //     .buf                   = text6,
+    //     .name                  = "zeta\0\0\0\0",
+    //     .ext                   = "txt",
+    //     .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+    //     .buffer_size           = 208,
+    // };
+    // write(request6);
 
 
 
